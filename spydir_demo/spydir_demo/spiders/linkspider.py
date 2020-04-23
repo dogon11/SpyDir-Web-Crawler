b66f6b7d0931ca8d0a1964000a0a12fe24f3c5e6
dogon11 <justinthree@gmail.com>
2020-04-23 01:47:55 -0400

    Update linkspider.py

--- spydir_demo/spydir_demo/spiders/linkspider.py
+++ spydir_demo/spydir_demo/spiders/linkspider.py
@@ -6,37 +6,15 @@ from scrapy.linkextractors import LinkExtractor
 from ..items import LinkScrapeItem
 from scrapy.spiders import Rule, CrawlSpider
 
-ALLOWED_DOMAINS = ['mason.gmu.edu/~abenavi']
-BROAD = False
-
-class LinkspiderSpider(CrawlSpider):
+class LinkSpider(CrawlSpider):
     name = "linkspider"
-    start_urls = ['http://mason.gmu.edu/~abenavi/index']
-    allowed_domains = ['mason.gmu.edu']
-    """ if not BROAD:
-        allowed_domains = ALLOWED_DOMAINS """
-    rules = [Rule(LinkExtractor(canonicalize=True, unique=True), callback="parse_items")]
+    rules = [Rule(LinkExtractor(canonicalize=True, unique=True), callback="parse_items")] #Gets all links in a webpage, calling parse_items.
+    link_items = [] #Needs to exist in some form. Used to retrieve list of dicts storing the links found and their web location.
     
     def parse_items(self, response):
-        link_items = []
-        links = LinkExtractor(canonicalize=True, unique=True).extract_links(response)
+        links = LinkExtractor(canonicalize=True, unique=True).extract_links(response) #Extracts all links on the page.
         for link in links:
-            """
-            if not BROAD:
-                allowed_domain = False
-                for allowed_domain in ALLOWED_DOMAINS:
-                    if allowed_domain in link.url:
-                        allowed_domain = True
-                if allowed_domain:
-                    link_item = LinkScrapeItem()
-                    link_item.url_from = response.url
-                    link_item.url_to = link.url
-            else:
-                """
-            link_item = LinkScrapeItem()
+            link_item = LinkScrapeItem() #Produces an Item that contains the scraped link to fill out fields.
             link_item['url_from'] = response.url
             link_item['url_to'] = link.url
-            print(response.url)
-            print(link.url)
-            link_items.append(link_item)
-        return link_items
+            self.link_items.append(link_item) #Appends to returnable list.
