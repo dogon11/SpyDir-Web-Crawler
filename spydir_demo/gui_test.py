6b02899093d72c34ff03b4011f57d0e5ae0c7f9b
dogon11 <justinthree@gmail.com>
2020-04-28 03:12:50 -0400

    RC1!
    We've hit Release Candidate 1, y'all!

--- spydir_demo/gui_test.py
+++ spydir_demo/gui_test.py
@@ -1,14 +1,9 @@
-import crochet
-crochet.setup()
-
-from tkinter import *
+import tkinter as tk
 import scrape_driver
 import tree_visualization
 from tkinter.filedialog import askopenfilename
 from functools import partial
-from spydir_demo.spiders.linkspider import LinkSpider
-from scrapy.crawler import CrawlerProcess, CrawlerRunner, Crawler
-from subprocess import Popen, STARTUPINFO
+from subprocess import Popen
 from shlex import quote
 from os import name as osname
 from os.path import basename
@@ -19,7 +14,9 @@ HAS_BEEN = False
 
 def viewData():
 	csv_path = quote(basename(askopenfilename()))
-	if osname == 'nt':
+	if csv_path == "''":
+		return
+	elif osname == 'nt':
 		Popen([csv_path], shell=True)
 	else:
 		Popen(["open", csv_path], shell=True)
@@ -35,11 +32,15 @@ def genList(filename):
 def changeFilename(listButtons):
 	global FILE_NAME
 	global HAS_BEEN
-	try:
+	file_store = FILE_NAME
+	try:		
 		FILE_NAME = scrape_driver.getFile()
 	except FileNotFoundError:
-		FILE_NAME = FILE_NAME
+		FILE_NAME = file_store
 	finally:
+		if FILE_NAME is None:
+			FILE_NAME = file_store
+			return
 		genList((FILE_NAME + ".csv"))	
 		if not HAS_BEEN:
 			for item in listButtons:
@@ -58,7 +59,7 @@ def genClick(e1, e2, e3, genLabel, gendLabel, default1, default2, root, hideLabe
 	if FILE_NAME == "Enter filename for .csv (Default: output)":
 		FILE_NAME = "output"
 	if(arg1==default1 or arg2==default2):	#tried to make a check for invalid params but couldnt get this to work right
-		warnLabel = Label(root, text="Enter parameters!")
+		warnLabel = tk.Label(root, text="Enter parameters!")
 		warnLabel.pack()
 	else:
 		input_urls = [arg1] #This has to be a list or scrapy breaks
@@ -70,34 +71,59 @@ def genClick(e1, e2, e3, genLabel, gendLabel, default1, default2, root, hideLabe
 		for show in showLabels:
 			show.pack()
 
+def runSearch(trueLabel, falseLabel, urlEntry, filename):
+	url = urlEntry.get()
+	if scrape_driver.search(url, filename):		
+		falseLabel.pack_forget()
+		trueLabel.pack()
+	else:
+		trueLabel.pack_forget()
+		falseLabel.pack()
+
+def search():
+	global FILE_NAME
+	searchWindow = tk.Toplevel()
+	promptLabel = tk.Label(searchWindow, text="Input the FULL URL you want to search for:")
+	enter = tk.Entry(searchWindow, width = 100, borderwidth = 5) #searched url
+	trueLabel = tk.Label(searchWindow, text="That URL exists in the tree.")
+	falseLabel = tk.Label(searchWindow, text="That URL does not exist in the tree.")
+	runSearchButton = tk.Button(searchWindow, text="Search for URL", padx=50, command=partial(runSearch, trueLabel, falseLabel, enter, (FILE_NAME + ".csv")))
+	quitButton = tk.Button(searchWindow, text="Go back", command=searchWindow.destroy)
+	promptLabel.pack()
+	enter.pack()
+	runSearchButton.pack()	
+	quitButton.pack()
+	searchWindow.mainloop()
+
 def main():
-	root = Tk()
+	root = tk.Tk()
 	root.title("SpyDir ;))")
-	e1 = Entry(root, width = 100, borderwidth = 5) #start URL
-	e2 = Entry(root, width = 100, borderwidth = 5) #allowed domain
-	e3 = Entry(root, width = 100, borderwidth = 5) #CSV filename
+	e1 = tk.Entry(root, width = 100, borderwidth = 5) #start URL
+	e2 = tk.Entry(root, width = 100, borderwidth = 5) #allowed domain
+	e3 = tk.Entry(root, width = 100, borderwidth = 5) #CSV filename
 	e1.pack()
 	e2.pack()
 	e3.pack()
 
-	genLabel = Label(root, text="Generating...")
-	gendLabel = Label(root, text="Generated! :)")
-	treeLabel = Label(root, text="Tree:")	
-	orLabel = Label(root, text="Or:")
+	genLabel = tk.Label(root, text="Generating...")
+	gendLabel = tk.Label(root, text="Generated! :)")
+	#treeLabel = tk.Label(root, text="Tree:")	
+	orLabel = tk.Label(root, text="Or:")
 
-	viewButton = Button(root, text="View dataset", padx=50, command=viewData, bg="#ffffff")
-	treeButton = Button(root, text="View Tree", padx=50, command=viewTree, bg="#ffffff")
-	listButtons = [viewButton, treeButton]
-	loadButton = Button(root, text="Load .csv on system", padx = 50, command=partial(changeFilename, listButtons), bg="#ffffff")
-	quitButton = Button(root, text="Quit", command=root.destroy)
+	viewButton = tk.Button(root, text="View dataset", padx=50, command=viewData, bg="#ffffff")
+	treeButton = tk.Button(root, text="View Tree", padx=50, command=viewTree, bg="#ffffff")
+	searchButton = tk.Button(root, text="Search for URL", padx=50, command=search, bg="#ffffff")
+	listButtons = [viewButton, treeButton, searchButton]
+	loadButton = tk.Button(root, text="Load .csv on system", padx = 50, command=partial(changeFilename, listButtons), bg="#ffffff")
+	quitButton = tk.Button(root, text="Quit", command=root.destroy)
 
 	default1 = "Enter root domain"
 	default2 = "Enter domain restriction"
 	default3 = "Enter filename for .csv (Default: output)"
-	e1.insert(END, default1)
-	e2.insert(END, default2)
-	e3.insert(END, default3)
-	runSpiderButton = Button(root, text="Run Spider !", padx=50, command=partial(genClick, e1, e2, e3, genLabel, gendLabel, default1, default2, root, listButtons, listButtons), bg="#ffffff")
+	e1.insert(tk.END, default1)
+	e2.insert(tk.END, default2)
+	e3.insert(tk.END, default3)
+	runSpiderButton = tk.Button(root, text="Run Spider !", padx=50, command=partial(genClick, e1, e2, e3, genLabel, gendLabel, default1, default2, root, listButtons, listButtons), bg="#ffffff")
 	runSpiderButton.pack()
 	orLabel.pack()
 	loadButton.pack()
